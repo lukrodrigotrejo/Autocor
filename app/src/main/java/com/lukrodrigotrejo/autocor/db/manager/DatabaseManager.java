bdc33fc21d01b4356eb86f3a4c53c3e54a5677ea
Luca Rodrigo Trejo <lukrodrigotrejo@gmail.com>
2015-08-29 11:21:13 -0300

    Commit

--- app/src/main/java/com/lukrodrigotrejo/autocor/db/manager/DatabaseManager.java
+++ app/src/main/java/com/lukrodrigotrejo/autocor/db/manager/DatabaseManager.java
@@ -5,7 +5,6 @@ import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
